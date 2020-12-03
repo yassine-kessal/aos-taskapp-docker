ea560122461c00d8b36dba007239d3934a33a491
Yassine Kessal <yassinekessalmain@gmail.com>
2020-12-03 13:14:03 +0100

    add user_id column

--- database/migrations/2020_11_30_201238_create_tasks_table.php
+++ database/migrations/2020_11_30_201238_create_tasks_table.php
@@ -18,6 +18,7 @@ class CreateTasksTable extends Migration
             $table->boolean('completed');
             $table->text('description')->nullable();
             $table->string('title');
+            $table->integer('user_id');
             $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
         });
