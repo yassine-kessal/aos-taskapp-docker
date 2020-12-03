455131f79696c4ccaa2ec21d8c71f33644c80d74
Yassine Kessal <yassinekessalmain@gmail.com>
2020-12-03 11:19:39 +0100

    create clear:all command

--- routes/console.php
+++ routes/console.php
@@ -14,6 +14,13 @@ use Illuminate\Support\Facades\Artisan;
 |
 */
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+Artisan::command('clear:all', function() {
+    $this->call('cache:clear');
+    $this->call('config:clear');
+    $this->call('event:clear');
+    $this->call('optimize:clear');
+    $this->call('queue:clear');
+    $this->call('route:clear');
+    $this->call('view:clear');
+    $this->call('clear-compiled');
+})->describe('clear all caches');
